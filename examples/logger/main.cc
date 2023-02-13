8b065d388a4f271d554c96077a53c16e91442030
awmthink <awmthink@outlook.com>
2023-02-13 20:25:45 +0800

    chore: redesign the project build structure

---
+++ examples/logger/main.cc
@@ -0,0 +1,10 @@
+#include <spdlog/spdlog.h>
+
+int main() {
+  spdlog::set_pattern("%^[%l][%m/%d-%H:%M:%S.%e][#%t][cpp_tests][%s:%#] %v %$");
+  SPDLOG_TRACE("this is a trace level log ");
+  SPDLOG_INFO("this is an info level log");
+  SPDLOG_WARN("this is an warn level log");
+  SPDLOG_DEBUG("this is an debug level log");
+  SPDLOG_CRITICAL("this is an critical level log");
+}
--- playground/main.cc
+++ playground/main.cc
@@ -1,6 +1,45 @@
+#include <iostream>
+#include <span>
+#include <string_view>
+#include <vector>
+
 #include "lib.h"
 
+void PrintStr(std::string_view str) { std::cout << str << std::endl; }
+
+void PrintArray(std::span<int> sp) {
+  for (auto n : sp) {
+    std::cout << n << std::endl;
+  }
+}
+
+template <typename T>
+constexpr auto GetTypeName() {
+  constexpr auto prefix = std::string_view{"[with T = "};
+  constexpr auto suffix = "]";
+  constexpr auto function = std::string_view{__PRETTY_FUNCTION__};
+
+  const auto start = function.find(prefix) + prefix.size();
+  const auto end = function.find_last_of(suffix);
+  const auto size = end - start;
+
+  return function.substr(start, size);
+}
+
+template <typename T>
+void PrintTypeName(T&& arg) {
+  std::cout << "type(T): " << GetTypeName<T>() << ", type(arg)："
+            << GetTypeName<decltype(arg)>() << std::endl;
+}
+
+template <typename T>
+void f(T&& arg) {
+  PrintTypeName(arg);
+  PrintTypeName(std::decay_t<T>(arg));
+}
+
 int main(int argc, char* argv[]) {
-  hello();
+  f("Hello");
   return 0;
 }
+
